ad459f461fd839b3500215d4cdf685e82143d34c
skalswn <92147848+skalswn@users.noreply.github.com>
2021-10-20 13:33:44 +0900

    dtd

--- LibraryManagement/src/LIbrary/BookMangerment.java
+++ LibraryManagement/src/LIbrary/BookMangerment.java
@@ -1,5 +1,38 @@
 package LIbrary;
 
 public class BookMangerment {
+	
+	Dao dao =new Dao();
+	int cnt;
+	String b_id;
+	String b_title;
+	String b_author;
+	String b_publisher;
+	int b_price;
+	
+	public Dao BookManagement() {
+		
+	}
+		
+	public void addBook(Bookvo) {
+		
+		
+	cnt=dao.insert(b_id,b_title,b_author,b_publisher,b_price);	
+	}
+		
+	
+	public String selectBook(String) {
+		
+	}
+	
+	public void selectAll() {
+		
+	}
+	
+	public void updateBook(String, int) {
+		
+	}
+	public void deleteBook(String) 
+
 
 }
